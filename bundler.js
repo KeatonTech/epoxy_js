cb65c8e5329f12a0143ad6dbb611197a194c4e98
Keaton Brandt <keaton.brandt@gmail.com>
2018-05-05 14:22:34 -0700

    Move operators back in to this NPM module, but under the /operators directory. Inspired by rxjs's build system

---
+++ bundler.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+
+// List of packages to create to wire up NPM publication.
+const packages = ["operators"];
+
+packages.forEach((packageName) => {
+    const packagePath = path.resolve(__dirname, packageName);
+    if (!fs.existsSync(packagePath)) {
+        fs.mkdirSync(packagePath);
+    }
+
+    fs.writeFileSync(
+        path.resolve(packagePath, 'index.js'),
+        ` "use strict";
+        Object.defineProperty(exports, "__esModule", { value: true });
+        function __export(m) {
+            for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+        }
+        __export(require("../dist/${packageName}.js"));
+        `
+    );
+
+    fs.writeFileSync(
+        path.resolve(packagePath, 'index.d.ts'),
+        `export * from '../dist/${packageName}';`
+    );
+});
