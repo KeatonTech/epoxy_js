abbc49091a2004c98666738b42f30a2c90ae9836
Keaton Brandt <keaton.brandt@gmail.com>
2018-08-20 12:23:36 -0400

    Adds a non-debug mutation serializer

--- serializer.ts
+++ serializer.ts
@@ -1,6 +1,9 @@
 import {installCoreExtensions} from './src/serializer/extensions/core';
 installCoreExtensions();
 
+import {installMutationExtensions} from './src/serializer/extensions/mutations';
+installMutationExtensions();
+
 export {installSerializerExtension, resetSerializerExtensions} from './src/serializer/extensions';
 export {encode, decode} from './src/serializer/serializer';
 export {SerializerExtension} from './src/serializer/types';
---
+++ src/serializer/extensions/mutations.ts
@@ -0,0 +1,119 @@
+import { encode, decode } from "../serializer";
+import { installSerializerExtension } from "../extensions";
+import { SerializerExtension } from "../types";
+import { ArraySpliceMutation, Mutation, ValueMutation, PropertyMutation, SubpropertyMutation } from "../../../epoxy";
+
+
+/** Encodes ValueMutation objects */
+const EpoxyValueMutationSerializer: SerializerExtension = {
+    id: 'epxy_mv',
+    priority: 80,
+    test: (data) => data instanceof ValueMutation,
+
+    encode: (data) => {
+        const mutation = data as ValueMutation<any>;
+        const toEncode = [
+            mutation.id,
+            mutation.createdBy,
+            encode(mutation.oldValue),
+            encode(mutation.newValue),
+        ];
+        return JSON.stringify(toEncode);
+    },
+
+    decode: (encoded) => {
+        const data = JSON.parse(encoded);
+        const ret = new ValueMutation(decode(data[2]), decode(data[3]));
+        ret.id = data[0];
+        ret.createdBy = data[1];
+        return ret;
+    },
+};
+
+/** Encodes PropertyMutation objects */
+const EpoxyPropertyMutationSerializer: SerializerExtension = {
+    id: 'epxy_mp',
+    priority: 90,
+    test: (data) => data instanceof PropertyMutation,
+
+    encode: (data) => {
+        const mutation = data as PropertyMutation<any>;
+        const toEncode = [
+            mutation.id,
+            mutation.createdBy,
+            mutation.key,
+            encode(mutation.oldValue),
+            encode(mutation.newValue),
+        ];
+        return JSON.stringify(toEncode);
+    },
+
+    decode: (encoded) => {
+        const data = JSON.parse(encoded);
+        const ret = new PropertyMutation(data[2], decode(data[3]), decode(data[4]));
+        ret.id = data[0];
+        ret.createdBy = data[1];
+        return ret;
+    },
+};
+
+/** Encodes SubpropertyMutation objects */
+const EpoxySubpropertyMutationSerializer: SerializerExtension = {
+    id: 'epxy_ms',
+    priority: 90,
+    test: (data) => data instanceof SubpropertyMutation,
+
+    encode: (data) => {
+        const mutation = data as SubpropertyMutation<any>;
+        const toEncode = [
+            mutation.id,
+            mutation.createdBy,
+            mutation.key,
+            encode(mutation.mutation),
+        ];
+        return JSON.stringify(toEncode);
+    },
+
+    decode: (encoded) => {
+        const data = JSON.parse(encoded);
+        const ret = new SubpropertyMutation(data[2], decode(data[3]));
+        ret.id = data[0];
+        ret.createdBy = data[1];
+        return ret;
+    },
+};
+
+/** Encodes ListSpliceMutation objects */
+const EpoxyArraySpliceMutationSerializer: SerializerExtension = {
+    id: 'epxy_ma',
+    priority: 90,
+    test: (data) => data instanceof ArraySpliceMutation,
+
+    encode: (data) => {
+        const mutation = data as ArraySpliceMutation<any>;
+        const toEncode = [
+            mutation.id,
+            mutation.createdBy,
+            mutation.key,
+            encode(mutation.deleted),
+            encode(mutation.inserted),
+        ];
+        return JSON.stringify(toEncode);
+    },
+
+    decode: (encoded) => {
+        const data = JSON.parse(encoded);
+        const ret = new ArraySpliceMutation(data[2], decode(data[3]), decode(data[4]));
+        ret.id = data[0];
+        ret.createdBy = data[1];
+        return ret;
+    },
+};
+
+/** Install the serializer hooks. */
+export function installMutationExtensions() {
+    installSerializerExtension(EpoxyValueMutationSerializer);
+    installSerializerExtension(EpoxyPropertyMutationSerializer);
+    installSerializerExtension(EpoxySubpropertyMutationSerializer);
+    installSerializerExtension(EpoxyArraySpliceMutationSerializer);
+}
